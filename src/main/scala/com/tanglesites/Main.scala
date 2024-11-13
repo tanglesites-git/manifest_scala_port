7ffdebd43638d46c744034ec3f672c1dbd7fcba8
Joseph <tanglesites@hotmail.com>
2024-11-13 08:10:55 -0600

    Add ManifestPropertyService

--- src/main/scala/com/tanglesites/Main.scala
+++ src/main/scala/com/tanglesites/Main.scala
@@ -1,20 +1,16 @@
 package com.tanglesites
 
-import com.tanglesites.ManifestHttpClient.close
-import com.tanglesites.ManifestHttpClient.get_Manifest
-import com.tanglesites.Common.*
-import com.tanglesites.ManifestRoot.*
-import upickle.default.*
+import com.tanglesites.ManifestPropertyService.*
 
 @main def main(): Unit =
   println("Hello world!")
-  val manifestValue: ujson.Value = get_Manifest(Environments.DEVELOPMENT)
-  val Root: ManifestRoot = read[ManifestRoot](os.read(FilePaths.MANIFEST))
-  println(Root.Response.version)
-  println(Root.Response.mobileWorldContentPaths.en)
-  println(Root.Response.mobileWorldContentPaths.ptBr)
-  println(Root.Response.jsonWorldContentPaths.en)
-  println(
-    Root.Response.jsonWorldComponentContentPaths.en.DestinyDamageTypeDefinition
+  val name = MobileWorldContentPathLanguage("en")
+  val name1 = JsonWorldContentPathLanguage("en")
+  val tableDict = JsonWorldComponentContentPaths("en")
+  val tablePath = JsonWorldComponentContentTables(
+    DestinyTableNames.DestinyInventoryItemDefinition,
+    "en"
   )
-  close()
+  println(name)
+  println(name1)
+  println(tablePath)
